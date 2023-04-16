d8b3b168990ac9daa2083d5c5cba743de65e2787
danieltavares1301 <daniel.tavares.1301@gmail.com>
2023-04-16 15:20:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.json({mensagem: 'Rota Funcinando'})
+})
+
+app.listen(8888, ()=>{
+    console.log('Meu servidor está rodando...')
+})
