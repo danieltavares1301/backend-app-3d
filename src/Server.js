a8a067b3378451c3357269003f19a071c4fd6062
danieltavares1301 <daniel.tavares.1301@gmail.com>
2023-04-17 12:15:55 -0300

    feat: coneção com o mongo atlas

---
+++ src/Server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+//const ScheduleRouter = require('./router/ScheduleRouter.js');
+const cors = require('cors');
+const mongoose = require('mongoose');
+//const { default: helmet } = require('helmet');
+
+dotenv.config();
+
+const app = express();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => console.log('Database Connected...'))
+  .catch(error =>
+    console.error('Error to connected to database: ' + error.message),
+  );
+
+app.use(express.json());
+
+app.use(cors());
+
+//app.use(helmet());
+
+//app.use(ScheduleRouter);
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express();
+const app = require('./server.js');
 const dotenv = require('dotenv');
 
 dotenv.config();
